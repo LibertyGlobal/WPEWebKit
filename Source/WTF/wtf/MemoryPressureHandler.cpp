67d402932d034e6bdea384f703a855f8c951812d
tomasz-karczewski-red <tomasz.karczewski@redembedded.com>
2024-06-18 16:22:57 +0200

    ARRISEOS-45892 set memory thresholds
    using values for ios

--- Source/WTF/wtf/MemoryPressureHandler.cpp
+++ Source/WTF/wtf/MemoryPressureHandler.cpp
@@ -40,7 +40,7 @@ namespace WTF {
 
 WTF_EXPORT_PRIVATE bool MemoryPressureHandler::ReliefLogger::s_loggingEnabled = false;
 
-#if PLATFORM(IOS_FAMILY)
+#if 1 // PLATFORM(IOS_FAMILY)
 static const double s_conservativeThresholdFraction = 0.5;
 static const double s_strictThresholdFraction = 0.65;
 #else
@@ -48,7 +48,7 @@ static const double s_conservativeThresholdFraction = 0.8;
 static const double s_strictThresholdFraction = 0.9;
 #endif
 static const std::optional<double> s_killThresholdFraction;
-static const Seconds s_pollInterval = 30_s;
+static const Seconds s_pollInterval = 10_s;
 
 // This file contains the amount of video memory used, and will be filled by some other
 // platform component. It's a text file containing an unsigned integer value.
