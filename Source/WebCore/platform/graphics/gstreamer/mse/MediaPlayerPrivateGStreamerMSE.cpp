WebKit/WebKit

[MSE][GStreamer] Prevent currentTime reset on preroll when audio sink is non-async https://bugs.webkit.org/show_bug.cgi?id=269587
Reviewed by Xabier Rodriguez-Calvar.

When doing an initial seek (during preroll) on an MSE content that is
audio-only on platforms whose audio sink is not asynchronous (eg: on
Broadcom Nexus devices), the manually triggered preroll introduced by
https://commits.webkit.org/268845@main causes an invalidation of the
currentTime cached position. This causes a temporary currentTime 0 value
that creates a drop to HaveMetadata in the MediaSource readyState,
causing a number of problems.

See: https://github.com/WebPlatformForEmbedded/WPEWebKit/pull/1211

This patch remembers seek position as a cached value on an initial seek
during preroll when the audio sink can't perform asynchronous state
changes.

Based on a patch by Jacek Skiba <jacek.skiba@consult.red>

* Source/WebCore/platform/graphics/gstreamer/mse/MediaPlayerPrivateGStreamerMSE.cpp:
(WebCore::MediaPlayerPrivateGStreamerMSE::doSeek): Cache the previous position (the seek position) before the manual preroll and then immediately propagate the readyState to the player and invalidate the cached position after the preroll, so the new position is the one resulting from the preroll to the seek target position.

Canonical link: https://commits.webkit.org/274971@main

Source/WebCore/platform/graphics/gstreamer/mse/MediaPlayerPrivateGStreamerMSE.cpp
@@ -211,7 +211,14 @@ bool MediaPlayerPrivateGStreamerMSE::doSeek(const SeekTarget& target, float rate
             if (!audioSinkPerformsAsyncStateChanges) {
                 // If audio-only pipeline's sink is not performing async state changes
                 // we must simulate preroll right away as otherwise nothing will trigger it.
+                bool mustPreventPositionReset = m_isWaitingForPreroll && m_isSeeking;
+                if (mustPreventPositionReset)
+                    m_cachedPosition = currentTime();
                 didPreroll();
+                if (mustPreventPositionReset) {
+                    propagateReadyStateToPlayer();
+                    invalidateCachedPosition();
+                }
             }
         }
     });

LibertyGlobal/WPEWebKit

Source/WebCore/platform/graphics/gstreamer/mse/MediaPlayerPrivateGStreamerMSE.cpp
@@ -276,7 +276,14 @@ bool MediaPlayerPrivateGStreamerMSE::doSeek(const MediaTime& position, float rat
             m_player->queueTaskOnEventLoop([weakThis = WeakPtr { *this }, this] {
                 if (!weakThis)
                     return;
+                bool mustPreventPositionReset = m_isWaitingForPreroll && m_isSeeking;
+                if (mustPreventPositionReset)
+                    m_cachedPosition = currentMediaTime();
                 didPreroll();
+                if (mustPreventPositionReset) {
+                    propagateReadyStateToPlayer();
+                    invalidateCachedPosition();
+                }
             });
         }
     }