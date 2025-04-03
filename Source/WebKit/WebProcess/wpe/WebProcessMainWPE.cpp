ae4d479bb0fac7c2ead4fdb72fd4e8d2345c8469
tomasz-karczewski-red <tomasz.karczewski@redembedded.com>
2025-04-03 14:12:22 +0200

    wtf todo

--- Source/WebKit/WebProcess/wpe/WebProcessMainWPE.cpp
+++ Source/WebKit/WebProcess/wpe/WebProcessMainWPE.cpp
@@ -73,8 +73,11 @@ public:
     }
 };
 
+void *ARGV_0_POINTER = nullptr;
+
 int WebProcessMain(int argc, char** argv)
 {
+    ARGV_0_POINTER = &(argv[0]);
     return AuxiliaryProcessMain<WebProcessMainWPE>(argc, argv);
 }
 
